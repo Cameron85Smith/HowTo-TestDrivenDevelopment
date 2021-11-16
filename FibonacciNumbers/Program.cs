b7e8725256bafb243c60bc108a76f11cbdbc7193
Cameron Smith <cameron85smith@gmail.com>
2021-11-16 16:17:15 +0200

    Removed comment

--- FibonacciNumbers/Program.cs
+++ FibonacciNumbers/Program.cs
@@ -1,11 +1,7 @@
 ﻿using NUnit.Framework;
 
-// Triangulation is when you drive the implementation by writing tests.
-// This means that we generalize the implementation after we have at
-// least two test cases.
 namespace FibonacciNumber
 {
-
     [TestFixture]
     public class FibonacciNumber
     {
@@ -13,13 +9,13 @@ namespace FibonacciNumber
         [TestCase(1, 1)]
         [TestCase(1, 2)]
         [TestCase(2, 3)]
-        public void TestFibonacciNumbers(int expected, int index) => Assert.AreEqual(expected, GetFibonacci(index));
+        public void TestFibonacciNumbers(int expected, int index) => Assert.AreEqual(expected, GetFibonacciNumber(index));
 
-        private int GetFibonacci(int index) => index switch
+        private int GetFibonacciNumber(int index) => index switch
         {
             0 => 0,
             1 => 1,
-            _ => GetFibonacci(index - 1) + GetFibonacci(index - 2)
+            _ => GetFibonacciNumber(index - 1) + GetFibonacciNumber(index - 2)
         };
     }
 }
