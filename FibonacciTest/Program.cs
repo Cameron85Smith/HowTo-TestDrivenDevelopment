fc58e1fef19b447e79c080563251e489b48e38a3
Cameron Smith <cameron85smith@gmail.com>
2021-11-24 10:47:04 +0200

    Added Refactored Fibonacci

---
+++ FibonacciTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace FibonacciTest
+{
+    [TestFixture]
+    public class FibonacciTest
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        public void TestFibonacci(int expected, int index) => Assert.AreEqual(expected, Fibonacci(index));
+
+        private int Fibonacci(int index) => index switch
+        {
+            0 => 0,
+            1 => 1,
+            _ => Fibonacci(index - 1) + Fibonacci(index - 2)
+        };
+    }
+}
