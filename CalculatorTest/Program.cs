bbe8062138d91e39e8aadac9ad19eee2ecd4bde4
Cameron Smith <cameron85smith@gmail.com>
2022-02-27 16:10:12 +0200

    Added Calculator solution

---
+++ CalculatorTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        // 1) - Develop the Unit Test
+        // 3) Refactor the logic.
+        [Test]
+        public void AddNumbers_ValidValues_ReturnsCorrectResult()
+        {
+            var addition = new Calculator();
+            int result = addition.AddNumbers(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        // 1) Develop the Unit Test
+        // 3) Refactor the logic.
+        [Test]
+        public void SubtractNumbers_ValidValues_ReturnsCorrectResults()
+        {
+            var subtraction = new Calculator();
+            int result  = subtraction.SubtractNumbers(5,3);
+            
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
